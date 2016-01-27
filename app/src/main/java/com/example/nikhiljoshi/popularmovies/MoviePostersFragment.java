d7d56d6a6bb7184007c37af909b2934a05d74d61
zenstateofmind <nikhil.joshi92@gmail.com>
2016-01-26 16:46:50 -0800

    Sample GridView with a bunch of sample images

--- app/src/main/java/com/example/nikhiljoshi/popularmovies/MoviePostersFragment.java
+++ app/src/main/java/com/example/nikhiljoshi/popularmovies/MoviePostersFragment.java
@@ -2,10 +2,14 @@ package com.example.nikhiljoshi.popularmovies;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -28,8 +32,17 @@ public class MoviePostersFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        final ImageView imageView = (ImageView) rootView.findViewById(R.id.image_view);
-        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+//        final ImageView imageView = (ImageView) rootView.findViewById(R.id.image_view);
+//        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getContext()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
 
